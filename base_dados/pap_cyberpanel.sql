eb7ecb81bdd257cd3deb5a5b26e5d56dbf4fb35d
João Reis <joaoreis@outlook.pt>
2016-04-09 20:44:51 +0100

    Better

--- base_dados/pap_cyberpanel.sql
+++ base_dados/pap_cyberpanel.sql
@@ -46,21 +46,6 @@ CREATE TABLE IF NOT EXISTS `servidores` (
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=42 ;
 
---
--- Extraindo dados da tabela `servidores`
---
-
-INSERT INTO `servidores` (`Id`, `IdUtilizador`, `TipoServidor`, `NomeServidor`, `Ip`, `Porta`, `PortaQuery`, `Status`, `DataHoraCriado`, `Instalado`, `MaxSlots`, `OnlineMode`, `Mapa`, `Motd`, `FTP_User`, `FTP_Password`) VALUES
-(15, 2, 1, 'Minecrafitas', '188.209.52.63', '25569', '25569', 0, '2015-09-22 14:40:20', 1, 5, 0, 'poop', 'Server_Exemplo', '15', 'RhsH0lRC'),
-(17, 1, 2, 'Servidor de counter strike 1.6', '188.209.52.63', '27015', '27015', 0, '2015-09-26 14:21:17', 1, 15, 0, 'de_dust2', 'Mudar Motd do Servidor', '17', 'sjQFx0rJ'),
-(18, 1, 2, 'Servidor para testes cs 1.6', '188.209.52.63', '27016', '27016', 0, '2015-09-26 18:14:24', 1, 3, 0, 'de_dust2', 'Este &eacute; o Motd do Servidor de CS', '18', 'Gj7B6cls'),
-(25, 2, 2, 'CS 1.6 Servidor LAN PARTY', '188.209.52.63', '5466', '5467', 0, '2015-11-24 08:57:47', 1, 10, 0, 'dust2', 'Servidor CS', '25', 'gpC9xbbF'),
-(27, 1, 1, 'Servidor de Minecraft', '188.209.52.63', '25565', '25565', 0, '2015-12-05 09:29:10', 1, 3, 0, 'mundinho', 'Teste_Servidor.', '27', '1MGheJrL'),
-(32, 13, 1, ' as9dhasds', '188.209.52.63', '8898', '8898', 0, '2016-01-07 19:37:11', 1, 6, 0, 'DreamWorld', 'Bem vindo ao fodÃ¡stico servidor de mine', '32', 'BDRhSOCR'),
-(35, 2, 1, 'Server Apresentacao', '188.209.52.63', '26656', '26656', 0, '2016-01-20 12:08:48', 1, 6, 0, 'mundo', 'Apresentacao', '35', 'gFqi1xms'),
-(37, 1, 1, 'Teste Local mine', '188.209.52.63', '26658', '26658', 0, '2016-01-20 13:00:12', 1, 5, 0, 'mundo', 'OlaTurma', '37', 'Euo4IJLb'),
-(40, 1, 3, 'Cs Go', '188.209.52.63', '27017', '27017', 0, '2016-02-26 11:26:10', 1, 10, 0, 'de_nuke', 'Sem MOTD', '40', '4hm3XOuH'),
-(41, 1, 3, 'Counter Strike GO teste', '188.209.52.63', '27018', '27018', 0, '2016-02-26 12:40:59', 1, 12, 0, 'de_dust2', 'Sem MOTD', '41', 'LmE3TCFC');
 
 -- --------------------------------------------------------
 
@@ -135,11 +120,7 @@ CREATE TABLE IF NOT EXISTS `utilizadores` (
 --
 
 INSERT INTO `utilizadores` (`Id`, `Nome`, `Apelido`, `Email`, `Password`, `DataHoraRegisto`, `TipoUtilizador`, `NumeroMaxServidores`, `IdTema`, `Linguagem`, `Cron`) VALUES
-(1, 'Jo&atilde;o', 'Reis', 'joaoreis@outlook.pt', 'abf7c9b735be7331ae133a32d8bdd894', '2015-09-15 14:00:00', 1, 14, 1, 'Portugues', 0),
-(2, 'Hugo', 'Rocha', 'hugo182011@gmail.com', '4e951936957c783062a399c629ce9a95', '2015-09-15 00:00:00', 1, 10, 1, 'Portugues', 0),
-(3, 'Sérgio', 'Reis', 'sergio_alvadia44@protonmail.ch', 'e4c51bee8f721e77aa203fe2d893cfa9 ', '2015-09-22 00:00:00', 0, 1, 1, 'Portugues', 0),
-(10, 'Administrador', 'Demo', 'admin@admin.com', 'abf7c9b735be7331ae133a32d8bdd894', '2015-10-29 18:58:23', 1, 3, 1, 'Portugues', 0),
-(13, 'demo', 'lels', 'demo@demo.com', 'fe01ce2a7fbac8fafaed7c982a04e229', '2016-01-07 19:27:50', 0, 2, 1, 'Portugues', 0);
+(1, 'Admin', 'System', 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3', '2015-09-15 14:00:00', 1, 14, 1, 'Portugues', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
